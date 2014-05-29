456722811a74ef272b37e7aa6b17cbb2367ba7e6
Angel <angel.talledo.tavara@gmail.com>
2014-05-29 16:39:07 -0500

    Validar Usuario
    Validar Usuario

--- verificar_login.php
+++ verificar_login.php
@@ -2,21 +2,23 @@
 require_once('Components/Config.conf');
 include 'Class/LoginClass.php';
 $objLoginClass = new Class_LoginClass();
-
 $objLoginClass->setName($_POST['name']);
 $objLoginClass->setPassword($_POST['password']);
 $result = $objLoginClass->registrarUser();
+$numRow = $result->num_rows;
 $row = $result->fetch_array();
-if(count($row )>0){
-    $_SESSION['usuario']=$row['pkCustomers'];
+if($numRow > 0){
+    $_SESSION['usuario'] = $row['pkCustomers'];
+    $_SESSION['US']= $row['customersfNames']." ".$row['firstName']." ".$row['lastName'];
     header("Location:index.php");
 }else{
-echo 'contraseña incorrecta';
-return;
+    $_SESSION['error']= 'Contraseña o Usuario Incorrecto';
+    echo('error');
+    return;
 }
- /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 ?>
