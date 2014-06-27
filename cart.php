8ef891a2a96d37df7d879948f4c7163872365d69
maxma00 <bryan_checa_100@hotmail.com>
2014-06-27 12:49:58 -0500

    Update cart.php

--- cart.php
+++ cart.php
@@ -170,7 +170,7 @@ if(isset($_SESSION['carrito'])){
 								</form>
 							</div>
 							<div class="cart_amount pull-right">
-									<p><a href="cart.php"><i class="fa fa-shopping-cart"></i> Cart (2)</a></p>
+									<p><a href="cart.php"><i class="fa fa-shopping-cart"></i> Cart (0)</a></p>
 							</div>
 						</div>
 					</div>
